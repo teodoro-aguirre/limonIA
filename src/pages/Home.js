8a1cfedf4c31c7506a916ce054a66ec2e3d54ae5
TEODORO DEL ANGEL RODRIGUEZ AGUIRRE <rodriguezaguirreangelt@gmail.com>
2021-08-18 01:20:04 -0500

    4. Se agregó iconografia, estilos y animaciones en Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,26 @@
 
 const Home = () => {
     const view = `
-        <h1>Home</h1>
+        <section class="home__animated">
+            <div class="home-container">
+                <div class="home-container__title">
+                    <h1>Limón IA APP</h1>
+                    <p>Detección de plagas y enfermedades de citrus latifolia mediante una aplicación 
+                    móvil con aprendizaje profundo a partir de redes neuronales convolucionales</p>
+                </div>
+                <div class="home-container__img">
+                    <img src="./assets/img/telefono-celular.png" alt="cel">
+                </div>
+            </div>
+            <div class="limones">
+                <div class="limon"></div>
+                <div class="limon-2"></div>
+                <div class="limon"></div>
+                <div class="limon-2"></div>
+                <div class="limon"></div>
+                <div class="limon-2"></div>
+            </div>
+        </section>
     `;
 
     return view;
--- src/template/Hearder.js
+++ src/template/Hearder.js
@@ -1,14 +1,36 @@
+import getHash from "../utils/getHash";
+
 // Plantilla de la barra de navegación
 const Header = () => {
-    const view = `
-        <ul>
-            <li><a href="/">Inicio</a></li>
-            <li><a href="#/contacto">Contacto</a></li>
-            <li><a href="#/descarga">Descarga</a></li>
-        </ul>
+  let hash = getHash();
+  const view = `
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/">
+                <img src="../assets/icon/logo5.svg" alt="" width="30" height="24">
+                Limon IA
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link  ${ hash == '/' ? 'active' : ''}" href="/">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link  ${ hash == '/contacto' ? 'active' : ''}" href="#/contacto">Contacto</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link  ${ hash == '/descarga' ? 'active' : ''}" href="#/descarga">Descarga</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
     `;
 
-    return view;
+  return view;
 };
 
 export default Header;
