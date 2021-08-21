3d2ef0f1725c755d2b540c74405dcfefde50b554
Angel Rodriguez Aguirre <rodriguezaguirreangelt@gmail.com>
2021-08-21 18:07:25 -0500

    13. Se agregó contenido del home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -22,12 +22,84 @@ const Home = () => {
                 <div class="limon"></div>
             </div>
         </section>
-        <section>
-            <div class="columns">
+        <section class="content_1 section">
+            <div class="columns is-vcentered">
                 <div class="column has-text-centered">
-                    <h2 class="title-section1">Hola</h2>
+                    <h2 class="title-section1">Soluciones de Inteligencia Artificial que aspira a transformar el sector agro tecnológico.</h2>
+                    <img src="./assets/icon/logo1.svg" class="img-fluid" width="250px" />
                 </div>
-                <div class="column ">
+                <div class="column">
+                <p class="content_text">Brindamos soluciones tecnológicas, apoyando a empresas, organizaciones e industrias a incursionar en la nueva economía de la transformación digital, mediante el desarrollo de software, y hardware a la medida.</p>
+                </div>
+            </div>
+        </section>
+        <section class="section content_2">
+            <div class="has-text-centered"><h1>Capturas de la aplicación</h1></div>
+            <div id="carouselCaptures" class="carousel slide " data-bs-ride="carousel">
+                <div class="carousel-inner contenedor_capturas">
+                    <div class="carousel-item active has-text-centered">
+                        <img src="./assets/captures/capture-login.jpeg" class="img-capture img-fluid" alt="...">
+                        <img src="./assets/captures/capture-home.jpeg" class="img-capture img-fluid" alt="...">
+                    </div>
+                    <div class="carousel-item has-text-centered">
+                        <img src="./assets/captures/capture-takephoto.jpeg" class="img-capture img-fluid" alt="...">
+                        <img src="./assets/captures/capture-result.jpeg" class="img-capture img-fluid" alt="...">
+                    </div>
+                    <div class="carousel-item has-text-centered">
+                        <img src="./assets/captures/capture-takephoto2.jpeg" class="img-capture img-fluid" alt="...">
+                        <img src="./assets/captures/capture-result-2.jpeg" class="img-capture img-fluid" alt="...">
+                    </div>
+                    <div class="carousel-item has-text-centered">
+                        <img src="./assets/captures/capture-menu.jpeg" class="img-capture img-fluid" alt="...">
+                        <img src="./assets/captures/capture-faqs.jpeg" class="img-capture img-fluid" alt="...">
+                    </div>
+                    
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselCaptures" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselCaptures" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </section>
+        <div class="content_servicio">
+            <div class="content_servicio-title">
+                <h1>Servicios</h1>
+            </div>
+        </div>
+        <section class="content_3 section">
+            <div class="has-text-centered content_3-text">
+                <h1>Detección de anomalías</h1>
+                <p>Podemos conocer el comportamiento de cada cultivo para cada día 
+                del año a través de inteligencia artificial. Esto nos permite identificar 
+                dónde el comportamiento del mismo no es adecuado identificando 
+                plagas, malezas, zonas no pobladas, etc.</p>
+            </div>
+            <div class="has-text-centered">
+                <a href="#/contacto" class="button_contact">Contáctanos</a>
+            </div>
+        </section>
+        <section class="section">
+            <div class="columns has-text-centered">
+                <div class="column">
+                    <h1 class="content_4-title">Visión</h1>
+                    <p class="content_4-content">Ser la empresa líder mundial en el procesado de inteligencia artificial 
+                    orientada a la agrotecnologia. Así, contribuir a una agricultura sostenible, que no sea dañina para 
+                    el medio ambiente y que ayude a cambiar el entorno social que la rodea</p>
+                </div>
+                <div class="column">
+                    <h1 class="content_4-title">Misión</h1>
+                    <p class="content_4-content">Dar servicio constante y no sentirnos proveedores si no una oficina de 
+                    tecnología experta en Tecnología IA que camina hombro con hombro 
+                    con nuestro cliente. </p>
+                </div>
+                <div class="column">
+                    <h1 class="content_4-title">Valores</h1>
+                    <p class="content_4-content">Pasión, trabajo duro, sentido de familia, compromiso, fidelidad, 
+                    profesionalidad, lealtad y honestidad</p>
                 </div>
             </div>
         </section>
