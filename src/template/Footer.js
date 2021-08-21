5d73cfaf5fdd17e31a676f3d5484f8092f54aebb
Angel Rodriguez Aguirre <rodriguezaguirreangelt@gmail.com>
2021-08-21 18:33:48 -0500

    15.  Se agregÂóipde página y libreria animate.css

--- src/template/Footer.js
+++ src/template/Footer.js
@@ -1,7 +1,15 @@
 // Plantilla que muestra el footer
 const Footer = () => {
     const view = `
-        <p>Footer</p>
+    <footer>
+        <div class="content has-text-centered">
+        <p>
+            <strong>Limon IA Web</strong> by <a href="https://github.com/teodoro-aguirre">Team Limon IA</a>. The source code is licensed
+            <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+            is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+        </p>
+        </div>
+    </footer>
     `;
 
     return view;
