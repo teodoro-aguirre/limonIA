435f37bb2a8affbcf7ad3307c61fd436721a6b2c
Angel Rodriguez Aguirre <rodriguezaguirreangelt@gmail.com>
2021-08-23 17:28:37 -0500

    21. Ventana de Descarta lista con estilos

--- src/pages/Download.js
+++ src/pages/Download.js
@@ -1,9 +1,25 @@
 // Pagina que mostrará la información acerca de la descarga de la app
 const Download = () =>{
     const view = `
-        <div class="container-download">
-        
-        </div>
+        <section class="container-download section">
+            <div class="has-text-centered container-download__content">
+                <h1 class="container-download__title">DESCARGA NUESTRA APP</h1>
+                <p class="container-download__text">Encuentra nuestra aplicación en las diferentes tiendas de aplicaciones.</p>
+                <p class="container-download__prox">Proximamente...</p>
+
+                <div class="columns">
+                    <div class="column">
+                        <img src="./assets/img/googlePlayStore.png" class="img-fluid img_app" alt="">
+                    </div>
+                    <div class="column">
+                        <img src="./assets/img/appStore.png" class="img-fluid img_app" alt="" width="200px">
+                    </div>
+                    <div class="column">
+                        <img src="./assets/img/appGallery.png" class="img-fluid img_app" alt="" width="200px">
+                    </div>
+                </div>
+            </div>
+        </section>
     `;
 
     return view;
