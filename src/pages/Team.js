68c1de715b963e54da8ad1cd55d923e7a85e5196
Angel Rodriguez Aguirre <rodriguezaguirreangelt@gmail.com>
2021-08-23 20:22:14 -0500

    22. SVentana de Team lista, se agregó iconografia

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -1,7 +1,62 @@
 
 const Team = () => {
     const view = `
-        <h1>Team</h1>
+        <section class="section">
+            <div class="has-text-centered container-team">
+                <h1>Conoce al equipo de Limon IA</h1>
+                <p>100% Mexicanos y estudiantes del Instituto Tecnologico Superior de Misantla</p>
+            </div>
+            <div class="columns has-text-centered">
+                <div class="column">
+                    <img src="./assets/img/alonso_hernandez_profile.jpeg" class="img-fluid img-profile" alt="Alonso Hernandez Mora">
+                    <p class="profile_name">Alonso Hernandez Mora</p>
+                    <a target="_blank" class="button" href="https://github.com/alonsohdzm25">
+                        <span class="icon">
+                        <i class="fab fa-github"></i>
+                        </span>
+                        <span>GitHub</span>
+                    </a>
+                    <a target="_blank" class="ml-2 button" href="https://www.linkedin.com/in/alonso-hernandez-mora/">
+                        <span class="icon">
+                        <i class="fab fa-linkedin"></i>
+                        </span>
+                        <span>Linkedin</span>
+                    </a>
+                </div>
+                <div class="column">
+                    <img src="./assets/img/teodoro_rodriguez_aguirre.jpeg" class="img-fluid img-profile" alt="Teodoro del Angel Rodriguez Aguirre">
+                    <p class="profile_name">Teodoro del Angel Rodriguez Aguirre</p>
+                    <a target="_blank" class="button" href="https://github.com/teodoro-aguirre">
+                        <span class="icon">
+                        <i class="fab fa-github"></i>
+                        </span>
+                        <span>GitHub</span>
+                    </a>
+                    <a target="_blank" class="ml-2 button" href="https://www.linkedin.com/in/teodoro-del-angel">
+                        <span class="icon">
+                        <i class="fab fa-linkedin"></i>
+                        </span>
+                        <span>Linkedin</span>
+                    </a>
+                </div>
+                <div class="column">
+                    <img src="./assets/img/luis_antonio_santiago.jpeg" class="img-fluid img-profile" alt="Luis Antonio Santiago Morales">
+                    <p class="profile_name">Luis Antonio Santiago Morales</p>
+                    <a target="_blank" class="button" href="https://github.com/gsabl3z">
+                        <span class="icon">
+                        <i class="fab fa-github"></i>
+                        </span>
+                        <span>GitHub</span>
+                    </a>
+                    <a target="_blank" class="ml-2 button" href="https://www.linkedin.com/in/luis-santiago-/">
+                        <span class="icon">
+                        <i class="fab fa-linkedin"></i>
+                        </span>
+                        <span>Linkedin</span>
+                    </a>
+                </div>
+            </div>
+        </section>
     `;
 
     return view;
